8933c546cbec5a17864dfe99f6f954f8b680dbd8
itsPeetah <pietro.g.moroni@gmail.com>
2025-01-25 09:00:30 +0100

    Starting cutscene

---
+++ Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using SPNK.Game.Events;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CutsceneManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct CutsceneSlide
+    {
+        public Sprite sprite;
+        public float duration;
+    }
+
+    [Header("Components")]
+    public Image slideShowScreen;
+
+    [Header("Data")]
+    public float startAfter = 1f;
+    public CutsceneSlide[] slides;
+    public string sceneToLoad = "";
+
+    [Header("Broadcast to")]
+    public VoidEventChannelSO onStart;
+    public VoidEventChannelSO onCutsceneStart;
+    public VoidEventChannelSO onNextSlide;
+
+    private void Start()
+    {
+        onStart.RaiseEvent();
+        StartCoroutine(DoSlideshow());
+        onCutsceneStart.RaiseEvent();
+    }
+
+
+    private IEnumerator DoSlideshow()
+    {
+        yield return new WaitForSeconds(startAfter);
+        for (int i = 0; i < slides.Length; i++)
+        {
+            yield return DoSlide(slides[i]);
+        }
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    private IEnumerator DoSlide(CutsceneSlide slide)
+    {
+        slideShowScreen.sprite = slide.sprite;
+        onNextSlide.RaiseEvent();
+        yield return new WaitForSeconds(slide.duration);
+    }
+
+}
